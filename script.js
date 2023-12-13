477db17dc687c0a7528b96a6740fb7ff12ed931b
Gabriel Sirvent <gsirvent@me.com>
2023-12-12 22:00:17 -0800

    cutting public info

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const userData = {
-        "login": "Dokjahyung",
-        "id": 117134089,
-        "node_id": "U_kgDOBvtTCQ",
         "avatar_url": "https://avatars.githubusercontent.com/u/117134089?v=4",
-        "gravatar_id": "",
         "url": "https://api.github.com/users/Dokjahyung",
         "html_url": "https://github.com/Dokjahyung",
         "followers_url": "https://api.github.com/users/Dokjahyung/followers",
@@ -16,34 +12,12 @@ document.addEventListener("DOMContentLoaded", function() {
         "repos_url": "https://api.github.com/users/Dokjahyung/repos",
         "events_url": "https://api.github.com/users/Dokjahyung/events{/privacy}",
         "received_events_url": "https://api.github.com/users/Dokjahyung/received_events",
-        "type": "User",
-        "site_admin": false,
         "name": "Gabriel Sirvent",
-        "company": null,
-        "blog": "",
         "location": "Fresno, California",
-        "email": null,
-        "hireable": null,
-        "bio": "I am shameless in how much I will work to get where I want to be 💯 ",
-        "twitter_username": null,
+        "email": "gsirvent@me.com",
         "public_repos": 16,
-        "public_gists": 0,
         "followers": 2,
         "following": 4,
-        "created_at": "2022-11-01T02:20:01Z",
-        "updated_at": "2023-12-09T06:20:28Z",
-        "private_gists": 0,
-        "total_private_repos": 0,
-        "owned_private_repos": 0,
-        "disk_usage": 165950,
-        "collaborators": 0,
-        "two_factor_authentication": false,
-        "plan": {
-        "name": "free",
-        "space": 976562499,
-        "collaborators": 0,
-        "private_repos": 10000
-        }
     };
 
     const userObject = JSON.parse(JSON.stringify(userData));
@@ -76,11 +50,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const twitter = document.createElement('p');
     twitter.textContent = `Twitter: ${userObject.twitter_username || 'Not available'}`;
 
-    const bio = document.createElement('div');
-    bio.className = 'bio';
-
-    const bioText = document.createElement('p');
-    bioText.textContent = userObject.bio;
 
         // Specific repositories to display (replace these with the repositories you want)
         const specificRepos = ['Naraka-AI', 'Dr.Bulba', 'dataStructuresMLB'];
@@ -116,8 +85,6 @@ document.addEventListener("DOMContentLoaded", function() {
     userInfo.appendChild(email); // Adding email
     userInfo.appendChild(twitter); // Adding Twitter handle
     container.appendChild(userInfo);
-    bio.appendChild(bioText);
-    container.appendChild(bio);
     repoList.appendChild(heading);
     repoList.appendChild(ul);
     container.appendChild(repoList);
